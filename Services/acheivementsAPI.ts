7a682073e4ca2677feaa718782440c899d1c7e35
Your Name <your.email@example.com>
2025-04-15 01:46:29 +0300

    sending email to subscribers

--- Services/acheivementsAPI.ts
+++
@@ -1,8 +0,0 @@
-import supabase from "./supabase";
-
-export default async function getAcheivements() {
-  let { data, error } = await supabase.from("achievements").select("*");
-  if (error) {
-  }
-  return data;
-}
